d15ba20efee89eac8dfb930ef08f787cd45ea063
gl.pinto10@uniandes.edu.co <gl.pinto10@ISIS2603S3-0016.sis.virtual.uniandes.edu.co>
2017-09-09 18:52:00 -0500

    Agregado el package para los test y clases Punto/Calificacion test vacias

---
+++ bicicletas-back/src/test/java/co/edu/uniandes/bicicletas/test/persistence/PuntoPersistenceTest.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.bicicletas.test.persistence;
+
+/**
+ *
+ * @author gl.pinto10
+ */
+public class PuntoPersistenceTest {
+    
+}
