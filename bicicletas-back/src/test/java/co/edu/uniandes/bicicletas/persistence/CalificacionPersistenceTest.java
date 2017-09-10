1250fd5c12c7e92d0bc3827629652a6c86ba9e1d
gl.pinto10@uniandes.edu.co <>
2017-09-09 19:14:56 -0500

    FINAL Carpeta test

---
+++ bicicletas-back/src/test/java/co/edu/uniandes/bicicletas/persistence/CalificacionPersistenceTest.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.bicicletas.persistence;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author gl.pinto10
+ */
+public class CalificacionPersistenceTest {
+    
+    public CalificacionPersistenceTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of create method, of class CalificacionPersistence.
+     */
+    @Test
+    public void testCreate() throws Exception {
+    }
+
+    /**
+     * Test of update method, of class CalificacionPersistence.
+     */
+    @Test
+    public void testUpdate() throws Exception {
+    }
+
+    /**
+     * Test of find method, of class CalificacionPersistence.
+     */
+    @Test
+    public void testFind() throws Exception {
+    }
+
+    /**
+     * Test of findAll method, of class CalificacionPersistence.
+     */
+    @Test
+    public void testFindAll() throws Exception {
+    }
+    
+}
