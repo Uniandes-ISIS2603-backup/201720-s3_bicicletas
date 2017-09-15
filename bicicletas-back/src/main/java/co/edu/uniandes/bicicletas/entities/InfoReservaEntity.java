878189bc77d07677e11338f6971e7804c7a1e0eb
rcasalla <rcasalla@uniandes.edu.co>
2017-09-15 08:03:09 -0500

    Se mueve de paquete de Perstence a Entity InfoReservaEntity Signed-off-by:rcasalla <rcasalla@uniandes.edu.co>

---
+++ bicicletas-back/src/main/java/co/edu/uniandes/bicicletas/entities/InfoReservaEntity.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.bicicletas.entities;
+
+import java.io.Serializable;
+import java.util.logging.Logger;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author ds.chacon
+ */
+class InfoReservaEntity extends BaseEntity implements Serializable{ 
+
+    
+}
