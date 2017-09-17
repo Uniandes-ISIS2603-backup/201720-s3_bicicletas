68117ea7fe2e5ce9b7733f5c010d123aaa6e2c14
ds.chacon <ds.chacon@ISIS2603S3-0029.sis.virtual.uniandes.edu.co>
2017-09-17 15:58:54 -0500

    Reserva Entity

--- bicicletas-back/src/main/java/co/edu/uniandes/bicicletas/entities/ReservaEntity.java
+++ bicicletas-back/src/main/java/co/edu/uniandes/bicicletas/entities/ReservaEntity.java
@@ -5,13 +5,64 @@
  */
 package co.edu.uniandes.bicicletas.entities;
 
+import java.util.Date;
+
 /**
  *
  * @author ds.chacon
  */
 public class ReservaEntity {
+    public static int CANCELADO=2; 
+    public static int PAGO=1;
+    public static int PAGADO=0; 
+    
+    int estado;
     long id;
+    Date FechaEntrega;
+    Date FechaInicio;
+    long idReserva;
+    double PrecioFinal;
 
+    public int getEstado() {
+        return estado;
+    }
+
+    public void setEstado(int estado) {
+        this.estado = estado;
+    }
+
+    public Date getFechaEntrega() {
+        return FechaEntrega;
+    }
+
+    public void setFechaEntrega(Date FechaEntrega) {
+        this.FechaEntrega = FechaEntrega;
+    }
+
+    public Date getFechaInicio() {
+        return FechaInicio;
+    }
+
+    public void setFechaInicio(Date FechaInicio) {
+        this.FechaInicio = FechaInicio;
+    }
+
+    public long getIdReserva() {
+        return idReserva;
+    }
+
+    public void setIdReserva(long idReserva) {
+        this.idReserva = idReserva;
+    }
+
+    public double getPrecioFinal() {
+        return PrecioFinal;
+    }
+
+    public void setPrecioFinal(double PrecioFinal) {
+        this.PrecioFinal = PrecioFinal;
+    }
+    
     public long getId() {
         return id;
     }
