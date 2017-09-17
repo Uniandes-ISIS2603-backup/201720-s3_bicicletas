f4bb267509e92c149fd653821918c7cece5a653b
js.torres1 <js.torres1@ISIS2603S3-0038.sis.virtual.uniandes.edu.co>
2017-09-17 18:32:21 -0500

    Accesorio Bicicleta entity

--- bicicletas-back/src/main/java/co/edu/uniandes/bicicletas/persistence/AccesorioBicicletaPersistence.java
+++ bicicletas-back/src/main/java/co/edu/uniandes/bicicletas/persistence/AccesorioBicicletaPersistence.java
@@ -31,12 +31,12 @@ public class AccesorioBicicletaPersistence {
      * @return devuelve la entidad creada con un id dado por la base de datos.
      */
     public AccesorioBicicletaEntity create(AccesorioBicicletaEntity entity) {
-        LOGGER.info("Creando un accesorioBicicleta nuevo");
+        LOGGER.info("Creando un AccesorioBicicleta nuevo");
         /* Note que hacemos uso de un método propio de EntityManager para persistir la Bicicleta en la base de datos.
         Es similar a "INSERT INTO table_codigo (column1, column2, column3, ...) VALUES (value1, value2, value3, ...);" en SQL.
          */
         em.persist(entity);
-        LOGGER.info("Creando un accesorioBicicleta nuevo");
+        LOGGER.info("Creando un AccesorioBicicleta nuevo");
         return entity;
     }
 
@@ -48,7 +48,7 @@ public class AccesorioBicicletaPersistence {
      * @return un bicicletas con los cambios aplicados.
      */
     public AccesorioBicicletaEntity update(AccesorioBicicletaEntity entity) {
-        LOGGER.log(Level.INFO, "Actualizando accesorioBicicleta con id={0}", entity.getId());
+        LOGGER.log(Level.INFO, "Actualizando AccesorioBicicleta con id={0}", entity.getId());
         /* Note que hacemos uso de un método propio del EntityManager llamado merge() que recibe como argumento
         la Bicicleta con los cambios, esto es similar a 
         "UPDATE table_codigo SET column1 = value1, column2 = value2, ... WHERE condition;" en SQL.
@@ -64,7 +64,7 @@ public class AccesorioBicicletaPersistence {
      * @param id: id correspondiente a la Bicicleta a borrar.
      */
     public void delete(Long id) {
-        LOGGER.log(Level.INFO, "Borrando Bicicleta con id={0}", id);
+        LOGGER.log(Level.INFO, "Borrando AccesorioBicicleta con id={0}", id);
         // Se hace uso de mismo método que esta explicado en public BicicletaEntity find(Long id) para obtener la Bicicleta a borrar.
         AccesorioBicicletaEntity entity = em.find(AccesorioBicicletaEntity.class, id);
         /* Note que una vez obtenido el objeto desde la base de datos llamado "entity", volvemos hacer uso de un método propio del
@@ -80,7 +80,7 @@ public class AccesorioBicicletaPersistence {
      * @return un bicicletas.
      */
     public AccesorioBicicletaEntity find(Long id) {
-        LOGGER.log(Level.INFO, "Consultando accesorioBicicleta con id={0}", id);
+        LOGGER.log(Level.INFO, "Consultando AccesorioBicicleta con id={0}", id);
         /* Note que se hace uso del metodo "find" propio del EntityManager, el cual recibe como argumento 
         el tipo de la clase y el objeto que nos hara el filtro en la base de datos en este caso el "id"
         Suponga que es algo similar a "select * from BicicletaEntity where id=id;" - "SELECT * FROM table_codigo WHERE condition;" en SQL.
