6815e3a38f634b27b033c46833c035da22395138
cm.alba10 <cm.alba10@ISIS2603S3-0020.sis.virtual.uniandes.edu.co>
2017-09-24 13:22:53 -0500

    implementado direccionDetailDTO

--- bicicletas-web/src/main/java/co/edu/uniandes/bicicletas/dtos/DireccionDetailDTO.java
+++ bicicletas-web/src/main/java/co/edu/uniandes/bicicletas/dtos/DireccionDetailDTO.java
@@ -6,6 +6,11 @@
 package co.edu.uniandes.bicicletas.dtos;
 
 import co.edu.uniandes.bicicletas.entities.DireccionEntity;
+import co.edu.uniandes.bicicletas.entities.EstacionEntity;
+import co.edu.uniandes.bicicletas.entities.UsuarioEntity;
+import java.util.ArrayList;
+import java.util.List;
+import static javax.ws.rs.client.Entity.entity;
 
 /**
  *
@@ -13,19 +18,42 @@ import co.edu.uniandes.bicicletas.entities.DireccionEntity;
  */
 public class DireccionDetailDTO extends DireccionDTO{
     
+    
+    // relación  cero o muchos usuarios
+    private List<UsuarioDTO> usuario;
+    
+    // relación  estacion
+    private List<EstacionDTO> estacion;
     /**
      * Constructor por defecto
      */
     public DireccionDetailDTO() {
+        super();
     }
-
+    
+    
     /**
      * Constructor para transformar un Entity a un DTO
      *
      * @param entity
      */
     public DireccionDetailDTO(DireccionEntity entity) {
-        super(entity);
+        if (entity.getUsuarios()!= null) {
+            usuario = new ArrayList<>();
+            for (UsuarioEntity entityUsuario : entity.getUsuarios()) {
+                usuario.add(new UsuarioDTO(entityUsuario));
+            }
+
+        }
+        
+        if (entity.getEstaciones()!= null) {
+        estacion = new ArrayList<>();
+        for (EstacionEntity entityEstacion : entity.getEstaciones()) {
+            estacion.add(new EstacionDTO(entityEstacion));
+        }
+
+    }
+        
     }
 
     /**
@@ -36,6 +64,41 @@ public class DireccionDetailDTO extends DireccionDTO{
     @Override
     public DireccionEntity toEntity() {
         DireccionEntity direccionE = super.toEntity();
-        return direccionE;
+    
+    if (usuario != null) {
+        List<UsuarioEntity> usuariosEntity = new ArrayList<>();
+        for (UsuarioDTO dtoUsuario : usuario) {
+            usuariosEntity.add(dtoUsuario.toEntity());
+        }
+        direccionE.setUsuarios(usuariosEntity);
     }
+    
+    if (estacion != null) {
+        List<EstacionEntity> estacionsEntity = new ArrayList<>();
+        for (EstacionDTO dtoEstacion : estacion) {
+            estacionsEntity.add(dtoEstacion.toEntity());
+        }
+        direccionE.setEstaciones(estacionsEntity);
+    }
+    
+    return direccionE;
+    }
+
+    public List<UsuarioDTO> getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(List<UsuarioDTO> usuario) {
+        this.usuario = usuario;
+    }
+
+    public List<EstacionDTO> getEstacion() {
+        return estacion;
+    }
+
+    public void setEstacion(List<EstacionDTO> estacion) {
+        this.estacion = estacion;
+    }
+    
+    
 }
