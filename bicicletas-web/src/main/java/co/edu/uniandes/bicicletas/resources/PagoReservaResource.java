2c78b10acedc934629126a67f2a19b7891765602
jd.trujillom <jd.trujillom@ISIS2603S3-0018.sis.virtual.uniandes.edu.co>
2017-09-25 14:01:27 -0500

    Servicios de pago terminados y funcionando

--- bicicletas-web/src/main/java/co/edu/uniandes/bicicletas/resources/PagoReservaResource.java
+++ bicicletas-web/src/main/java/co/edu/uniandes/bicicletas/resources/PagoReservaResource.java
@@ -22,10 +22,13 @@ import javax.inject.Named;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.ext.Provider;
+import co.edu.uniandes.bicicletas.ejb.SistemaDePagosLogic;
 
 /**
  * Clase que modela la relación que va desde Reserva hasta Pago. 
@@ -41,6 +44,9 @@ public class PagoReservaResource {
     
     @Inject
     private PagoLogic pagoLogic;
+    
+    private
+    SistemaDePagosLogic sistemaDePagos;
 
     @GET
     public PagoDetailDTO darPago(@PathParam("idReserva") Long idReserva){
@@ -60,16 +66,81 @@ public class PagoReservaResource {
         //En este metodo es una buena idea usar a pagologic porque se puede
         //utilizar el método para crear un pago y asociarselo a una reserva
         ReservaEntity reserva = reservaLogic.getReserva(idReserva);
+        pago.setEstado(PagoEntity.ESPERANDO_PAGO);
         
         if(reserva.getPago() != null)
            throw new BusinessLogicException("La reserva con id: " + idReserva + 
                    " ya tiene un pago asociado");
         
         reserva.setPago(pago);
-        reservaLogic.actualizarReserva(reserva);
-        
+        ReservaEntity nuevoReservaEntity = reservaLogic.actualizarReserva(reserva);
         
         return new PagoDetailDTO(pago);
     }
+    
+    @PUT
+    @Path("pagar")
+    public PagoDetailDTO efectuarPago(@PathParam("idReserva")Long idReserva)throws BusinessLogicException{
+        ReservaEntity reserva = reservaLogic.getReserva(idReserva);
+        PagoEntity pago = reserva.getPago();
+        
+        if(reserva.getPago() == null)
+           throw new BusinessLogicException("La reserva con id: " + idReserva + 
+                   " no tiene un pago asociado");
+ 
+        
+        if(pago.getEstado() != PagoEntity.ESPERANDO_PAGO){
+            throw new BusinessLogicException("No se puede realizar el pago en"
+                    + "el estado que se encuentra el pago");
+        }
+        
+        sistemaDePagos = new SistemaDePagosLogic();
+        
+        //Actualizar estado del pago 
+        pago.setEstado(PagoEntity.PROCESANDO_PAGO);
+        Long idTransaccion = sistemaDePagos.realizarPago();
+        pago.setIdTransaccion(idTransaccion);
+            if(reserva.getUsuarioReserva() != null)
+        pago.setIdUsuario(reserva.getUsuarioReserva().getId());
+        pagoLogic.updatePago(pago);
+        
+        //Actualizar estado de la reserva
+        reserva.setEstado(ReservaEntity.PAGO);
+        reservaLogic.actualizarReserva(reserva);
+        
+        return new PagoDetailDTO(pago);
+        
+    } 
+    
+    
+    @PUT
+    @Path(" ")
+    public PagoDetailDTO solicitarReembolso(@PathParam("idReserva")Long idReserva)throws BusinessLogicException{
+        ReservaEntity reserva = reservaLogic.getReserva(idReserva);
+        PagoEntity pago = reserva.getPago();
+        
+        if(reserva.getPago() == null)
+           throw new BusinessLogicException("La reserva con id: " + idReserva + 
+                   " no tiene un pago asociado");
+ 
+        
+        if(pago.getEstado() != PagoEntity.PAGADO){
+            throw new BusinessLogicException("No se puede realizar el pago en"
+                    + "el estado que se encuentra el pago");
+        }
+        
+        sistemaDePagos = new SistemaDePagosLogic();
+        
+        //Actualizar estado del pago 
+        pago.setEstado(PagoEntity.PROCESANDO_REEMBOLSO);
+        pagoLogic.updatePago(pago);
+        
+        //Actualizar estado de la reserva
+        reserva.setEstado(ReservaEntity.REENBOLSADO);
+        reservaLogic.actualizarReserva(reserva);
+        
+        return new PagoDetailDTO(pago);
+        
+    } 
     
 }
