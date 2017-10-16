6e7b6ff89e923d60b6486718eb515f349af2e042
jd.trujillom <jd.trujillom@ISIS2603S3-0018.sis.virtual.uniandes.edu.co>
2017-10-16 11:27:18 -0500

    Se corrigió un bug que no permitia consultar una reserva asociada a un pago por medio de la ruta /pagos/{id}/reserva.

--- bicicletas-back/src/main/java/co/edu/uniandes/bicicletas/entities/ReservaEntity.java
+++ bicicletas-back/src/main/java/co/edu/uniandes/bicicletas/entities/ReservaEntity.java
@@ -12,6 +12,7 @@ import java.util.Date;
 import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
@@ -68,7 +69,8 @@ public class ReservaEntity extends BaseEntity implements Serializable {
     @PodamExclude
     private EstacionEntity estacionSalida;
     
-    @OneToOne 
+    @OneToOne(fetch=FetchType.LAZY)
+    @JoinColumn(name="PAGO_ID")
     @PodamExclude
     private PagoEntity pago;
     
