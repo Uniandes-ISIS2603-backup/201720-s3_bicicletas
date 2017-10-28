aa4eec2c0e3765d7f6c4a025ef731b15028a20fc
cm.alba10 <cm.alba10@ISIS2603S3-0020.sis.virtual.uniandes.edu.co>
2017-10-28 17:39:17 -0500

    se pueden crear usuarios desde la web. solucionado problema del index linea 66

--- bicicletas-web/src/main/webapp/src/modules/usuarios/new/usuarios.new.ctrl.js
+++ bicicletas-web/src/main/webapp/src/modules/usuarios/new/usuarios.new.ctrl.js
@@ -1,8 +1,8 @@
 (function (ng) {
 	    var mod = ng.module("usuarioModule");
 	    mod.constant("usuariosContext", "api/usuarios");
-	    mod.controller('usuarioNewCtrl', ['$scope', '$http', 'usuariosContext', '$state', 'direccionsContext', '$rootScope',
-	        function ($scope, $http, usuariosContext, $state, direccionsContext, $rootScope) {
+	    mod.controller('usuarioNewCtrl', ['$scope', '$http', 'usuariosContext', '$state', 'direccionesContext', '$rootScope',
+	        function ($scope, $http, usuariosContext, $state, direccionesContext, $rootScope) {
 	            $rootScope.edit = false;
 	            $scope.createUsuario = function () {
 	                $http.post(usuariosContext, {
@@ -15,7 +15,7 @@
 	                    contraseniaPSE: $scope.usuarioContraseniaPSE
 	                }).then(function (response) {
 	                    //Usuario created successfully
-	                    $state.go('usuariosList', {documentoUsuario: response.data.documentoUsuario}, {reload: true});
+	                    $state.go('usuariosList', {UsuarioDocumentoUsuario: response.data.documentoUsuario}, {reload: true});
 	                });
 	            };
 	        }
