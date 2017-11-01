f5bacdac6e90a9be782527c06aec69e0c54a4451
ds.chacon <ds.chacon@ISIS2603S3-0029.sis.virtual.uniandes.edu.co>
2017-10-31 22:31:40 -0500

    Ya sirve el reservas detail

---
+++ bicicletas-web/src/main/webapp/src/modules/reservas/new/reservas.new.ctrl.js
@@ -0,0 +1,26 @@
+(function (ng) {
+	    var mod = ng.module("reservaModule");
+            mod.constant("reservasContext", "reservas");
+	    mod.constant("usuariosContext", "api/usuarios");
+	    mod.controller('reservaNewCtrl', ['$scope', '$http', 'usuariosContext', '$state','$rootScope','reservasContext',
+	        function ($scope, $http, usuariosContext, $state,$rootScope) {
+	            $rootScope.edit = false;
+	            $scope.createUsuario = function () {
+	                $http.post(usuariosContext+'/'+$scope.documentoUsuario+'/'+'reservas',{
+	                    idUsuario : $scope.documentoUsuario,
+	                    fechaInicio : $scope.fechaSalida+':00',
+	                    fechaEntrega: $scope.fechaLlegada+':00',
+                            estacionSalida: {
+                                id : $scope.estacionId
+                            }
+	                }).then(function (response) {
+	                    //Usuario created successfully
+	                    $state.go('usuariosList', {UsuarioDocumentoUsuario: response.data.documentoUsuario}, {reload: true});
+	                });
+	            };
+	        }
+	    ]);
+	}
+)(angular);
+
+
