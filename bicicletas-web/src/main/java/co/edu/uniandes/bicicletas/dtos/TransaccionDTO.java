ddf575a7b13a7ced72e25686c35635991b2236d2
Juan Diego Trujillo Montoya <jd.trujillom@uniandes.edu.co>
2017-11-07 13:01:31 -0500

    Errores menores solucionados

--- bicicletas-web/src/main/java/co/edu/uniandes/bicicletas/dtos/TransaccionDTO.java
+++ bicicletas-web/src/main/java/co/edu/uniandes/bicicletas/dtos/TransaccionDTO.java
@@ -59,4 +59,29 @@ public class TransaccionDTO {
         return entity;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(Integer tipo) {
+        this.tipo = tipo;
+    }
+
+    public Double getValor() {
+        return valor;
+    }
+
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
+
+    
 }
