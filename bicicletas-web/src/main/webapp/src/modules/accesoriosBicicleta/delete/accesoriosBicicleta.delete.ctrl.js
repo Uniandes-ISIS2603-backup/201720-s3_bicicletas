de8ec78b167a65a05a36601cbe5132be3e993a0e
glpinto10 <gl.pinto10@uniandes.edu.co>
2017-11-25 10:42:15 -0500

    Se crea correctamente un accesorio bicicleta y se puede eliminar

---
+++ bicicletas-web/src/main/webapp/src/modules/accesoriosBicicleta/delete/accesoriosBicicleta.delete.ctrl.js
@@ -0,0 +1,18 @@
+(function(ng){
+        var mod = ng.module("accesorioBicicletaModule");
+        mod.constant("accBicicletaContext","api/bicicletas");
+        mod.constant("accesorioBicicletasContext","accesorioBicicleta");
+        mod.controller('accesorioBicicletaDeleteCtrl',['$scope', '$http','accBicicletaContext', 'accesorioBicicletasContext', '$state',
+            function ($scope, $http, accesorioBicicletasContext,accBicicletaContext, $state) {
+	            $scope.deleteAccesorioBicicleta = function () {
+	                $http.delete(accesorioBicicletasContext + '/' +$state.params.id + '/' + accBicicletaContext + '/' + $state.params.idAccesorioBicicleta, {                           
+                        }).then(function (response) {
+	                    $state.go('accesoriosBicicletaBicicleta', {id: $state.params.id}, {reload: true});
+	                });
+	            };
+        }
+        ]);
+        
+    }
+)(angular);
+
