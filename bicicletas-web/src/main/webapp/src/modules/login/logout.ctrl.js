609f6482b5a56ce39555f6aee667be1626629d31
cm.alba10 <cm.alba10@ISIS2603S3-0020.sis.virtual.uniandes.edu.co>
2017-11-25 15:48:15 -0500

    se cierra alert

--- bicicletas-web/src/main/webapp/src/modules/login/logout.ctrl.js
+++ bicicletas-web/src/main/webapp/src/modules/login/logout.ctrl.js
@@ -8,6 +8,11 @@
             }
         }
     ]);
+    window.setTimeout(function() {
+    $(".alert").fadeTo(500, 0).slideUp(500, function(){
+        $(this).remove(); 
+    });
+    }, 1500);
 }
 )(window.angular);
 
