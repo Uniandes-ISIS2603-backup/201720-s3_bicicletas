d81d51b1a1830d6daa43348bd8e4f3a609443ad5
El-Kabs <ka.babativa@uniandes.edu.co>
2017-11-29 11:30:45 -0500

    Coma que faltaba

--- bicicletas-web/src/main/webapp/src/modules/accesorios/estacion/accesorios.estacion.new.ctrl.js
+++ bicicletas-web/src/main/webapp/src/modules/accesorios/estacion/accesorios.estacion.new.ctrl.js
@@ -1,7 +1,7 @@
 (function (ng) {
 var mod = ng.module("accesorioModule");
     mod.constant("estacionesAccesoriosContext", "api/accesorios");
-    mod.controller('accesoriosEstacionNewCtrl', ['$scope', '$http', 'estacionesAccesoriosContext', '$state'
+    mod.controller('accesoriosEstacionNewCtrl', ['$scope', '$http', 'estacionesAccesoriosContext', '$state',
         function ($scope, $http, estacionesAccesoriosContext, $state) {
             $scope.createAccesorio = function () {
 	                $http.post(estacionesAccesoriosContext + '/', {
